e7780714cda084b89930350c61bf02971521fe53
paolomandica <paolomandica.p@gmail.com>
2023-03-29 15:49:15 -0700

    Add new configs + minor fix

--- core/models/classifier.py
+++ core/models/classifier.py
@@ -273,7 +273,7 @@ class DepthwiseSeparableASPP_Hyper(nn.Module):
 
         # hyperbolic layer
         decoder_out = self.conv_reduce(decoder_out)
-        decoder_out = self.mapper(decoder_out)
+        decoder_out = self.mapper.expmap2(decoder_out)
         out = self.conv_seg(decoder_out)
 
         if size is not None:
