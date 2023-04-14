e99bd4ab64e8a85e8fd6f6d12dbe2ccd78327a8d
paolomandica <paolomandica.p@gmail.com>
2023-04-13 22:30:27 -0700

    Implement multiprocessing in region selection

--- core/active/build.py
+++ core/active/build.py
@@ -1,9 +1,11 @@
 import math
 import torch
+import random
 
 import numpy as np
 import torch.nn.functional as F
 
+from joblib import Parallel, delayed
 from PIL import Image
 from tqdm import tqdm
 from .floating_region import FloatingRegionScore
@@ -20,7 +22,6 @@ CITYSCAPES_MEAN = torch.Tensor([123.675, 116.28, 103.53]).reshape(1, 1, 3).numpy
 CITYSCAPES_STD = torch.Tensor([58.395, 57.12, 57.375]).reshape(1, 1, 3).numpy()
 
 np.random.seed(cfg.SEED+1)
-VIZ_LIST = list(np.random.randint(0, 500, 20))
 
 
 def PixelSelection(cfg, feature_extractor, classifier, tgt_epoch_loader):
@@ -128,98 +129,106 @@ def RegionSelection(cfg, feature_extractor, classifier, tgt_epoch_loader, round_
     purity_type = cfg.ACTIVE.PURITY
     alpha = cfg.ACTIVE.ALPHA
 
-    with torch.no_grad():
-        idx = 0
-        for tgt_data in tqdm(tgt_epoch_loader):
-            tgt_input, path2mask = tgt_data['img'], tgt_data['path_to_mask']
-            origin_mask, origin_label = \
-                tgt_data['origin_mask'], tgt_data['origin_label']
-            origin_size = tgt_data['size']
-            active_indicator = tgt_data['active']
-            selected_indicator = tgt_data['selected']
-            path2indicator = tgt_data['path_to_indicator']
+    # get list of files from dataset
+    data_list = tgt_epoch_loader.dataset.data_list
+    data_list = random.sample(data_list, 20)
+    data_list = [x['name'] for x in data_list]
 
-            tgt_input = tgt_input.cuda(non_blocking=True)
+    VIZ_SET = set(data_list)
 
-            tgt_size = tgt_input.shape[-2:]
-            tgt_feat = feature_extractor(tgt_input)
-            if not cfg.MODEL.HYPER:
-                tgt_out = classifier(tgt_feat, size=tgt_size)
-                decoder_out = None
+    def select_region(tgt_data):
+        tgt_input, path2mask = tgt_data['img'], tgt_data['path_to_mask']
+        origin_mask, origin_label = \
+            tgt_data['origin_mask'], tgt_data['origin_label']
+        origin_size = tgt_data['size']
+        active_indicator = tgt_data['active']
+        selected_indicator = tgt_data['selected']
+        path2indicator = tgt_data['path_to_indicator']
+
+        tgt_input = tgt_input.cuda(non_blocking=True)
+
+        tgt_size = tgt_input.shape[-2:]
+        tgt_feat = feature_extractor(tgt_input)
+        if not cfg.MODEL.HYPER:
+            tgt_out = classifier(tgt_feat, size=tgt_size)
+            decoder_out = None
+        else:
+            tgt_out, decoder_out = classifier(tgt_feat, size=tgt_size)
+
+        # just a single iteration, because len(origin_mask)=1
+        for i in range(len(origin_mask)):
+            active_mask = origin_mask[i].cuda(non_blocking=True)
+            ground_truth = origin_label[i].cuda(non_blocking=True)
+            size = (origin_size[i][0], origin_size[i][1])
+            num_pixel_cur = size[0] * size[1]
+            active = active_indicator[i]
+            selected = selected_indicator[i]
+
+            if cfg.ACTIVE.RATIO == 1:
+                active_mask = ground_truth
+                active = torch.ones_like(active_mask, dtype=torch.bool)
+                selected = torch.ones_like(active_mask, dtype=torch.bool)
             else:
-                tgt_out, decoder_out = classifier(tgt_feat, size=tgt_size)
+                output = tgt_out[i:i + 1, :, :, :]
+                output = F.interpolate(output, size=size, mode='bilinear', align_corners=True)
 
-            # just a single iteration, because len(origin_mask)=1
-            for i in range(len(origin_mask)):
-                active_mask = origin_mask[i].cuda(non_blocking=True)
-                ground_truth = origin_label[i].cuda(non_blocking=True)
-                size = (origin_size[i][0], origin_size[i][1])
-                num_pixel_cur = size[0] * size[1]
-                active = active_indicator[i]
-                selected = selected_indicator[i]
+                if uncertainty_type in ['certainty', 'hyperbolic'] or (uncertainty_type == 'none' and cfg.MODEL.HYPER) or (cfg.ACTIVE.ALPHA and cfg.MODEL.HYPER):
+                    decoder_out = decoder_out[i:i + 1, :, :, :]
+                    decoder_out = F.interpolate(decoder_out, size=size, mode='bilinear', align_corners=True)
 
-                if cfg.ACTIVE.RATIO == 1:
-                    active_mask = ground_truth
-                    active = torch.ones_like(active_mask, dtype=torch.bool)
-                    selected = torch.ones_like(active_mask, dtype=torch.bool)
-                else:
-                    output = tgt_out[i:i + 1, :, :, :]
-                    output = F.interpolate(output, size=size, mode='bilinear', align_corners=True)
+                score, purity, uncertainty = floating_region_score(
+                    output, decoder_out=decoder_out, normalize=cfg.ACTIVE.NORMALIZE, unc_type=uncertainty_type, pur_type=purity_type, alpha=alpha)
 
-                    if uncertainty_type in ['certainty', 'hyperbolic'] or (uncertainty_type == 'none' and cfg.MODEL.HYPER) or (cfg.ACTIVE.ALPHA and cfg.MODEL.HYPER):
-                        decoder_out = decoder_out[i:i + 1, :, :, :]
-                        decoder_out = F.interpolate(decoder_out, size=size, mode='bilinear', align_corners=True)
+                score[active] = -float('inf')
 
-                    score, purity, uncertainty = floating_region_score(
-                        output, decoder_out=decoder_out, normalize=cfg.ACTIVE.NORMALIZE, unc_type=uncertainty_type, pur_type=purity_type, alpha=alpha)
+                active_regions = math.ceil(num_pixel_cur * active_ratio / per_region_pixels)
 
-                    score[active] = -float('inf')
+                for pixel in range(active_regions):
+                    values, indices_h = torch.max(score, dim=0)
+                    _, indices_w = torch.max(values, dim=0)
+                    w = indices_w.item()
+                    h = indices_h[w].item()
 
-                    active_regions = math.ceil(num_pixel_cur * active_ratio / per_region_pixels)
+                    active_start_w = w - active_radius if w - active_radius >= 0 else 0
+                    active_start_h = h - active_radius if h - active_radius >= 0 else 0
+                    active_end_w = w + active_radius + 1
+                    active_end_h = h + active_radius + 1
 
-                    for pixel in range(active_regions):
-                        values, indices_h = torch.max(score, dim=0)
-                        _, indices_w = torch.max(values, dim=0)
-                        w = indices_w.item()
-                        h = indices_h[w].item()
+                    mask_start_w = w - mask_radius if w - mask_radius >= 0 else 0
+                    mask_start_h = h - mask_radius if h - mask_radius >= 0 else 0
+                    mask_end_w = w + mask_radius + 1
+                    mask_end_h = h + mask_radius + 1
 
-                        active_start_w = w - active_radius if w - active_radius >= 0 else 0
-                        active_start_h = h - active_radius if h - active_radius >= 0 else 0
-                        active_end_w = w + active_radius + 1
-                        active_end_h = h + active_radius + 1
+                    # mask out
+                    score[mask_start_h:mask_end_h, mask_start_w:mask_end_w] = -float('inf')
+                    active[mask_start_h:mask_end_h, mask_start_w:mask_end_w] = True
+                    selected[active_start_h:active_end_h, active_start_w:active_end_w] = True
+                    # active sampling
+                    active_mask[active_start_h:active_end_h, active_start_w:active_end_w] = \
+                        ground_truth[active_start_h:active_end_h, active_start_w:active_end_w]
 
-                        mask_start_w = w - mask_radius if w - mask_radius >= 0 else 0
-                        mask_start_h = h - mask_radius if h - mask_radius >= 0 else 0
-                        mask_end_w = w + mask_radius + 1
-                        mask_end_h = h + mask_radius + 1
+            active_mask_np = np.array(active_mask.cpu().numpy(), dtype=np.uint8)
+            active_mask_IMG = Image.fromarray(active_mask_np)
+            active_mask_IMG.save(path2mask[i])
+            indicator = {
+                'active': active,
+                'selected': selected
+            }
+            torch.save(indicator, path2indicator[i])
 
-                        # mask out
-                        score[mask_start_h:mask_end_h, mask_start_w:mask_end_w] = -float('inf')
-                        active[mask_start_h:mask_end_h, mask_start_w:mask_end_w] = True
-                        selected[active_start_h:active_end_h, active_start_w:active_end_w] = True
-                        # active sampling
-                        active_mask[active_start_h:active_end_h, active_start_w:active_end_w] = \
-                            ground_truth[active_start_h:active_end_h, active_start_w:active_end_w]
+        name = tgt_data['name'][0]
+        if cfg.ACTIVE.VIZ_MASK and name in VIZ_SET:
+            img_np = F.interpolate(tgt_input, size=size, mode='bilinear',
+                                   align_corners=True).cpu().numpy()[0].transpose(1, 2, 0)
+            img_np = (img_np * CITYSCAPES_STD + CITYSCAPES_MEAN).astype(np.uint8)
+            # active_mask_np = F.interpolate(torch.Tensor(active_mask_np).unsqueeze(0).unsqueeze(0), size=torch.Size(img_np.shape[:2]), mode='bilinear', align_corners=True)
+            # active_mask_np = active_mask_np.cpu().numpy().squeeze(0).squeeze(0)
+            score_np = score.cpu().numpy()
+            visualization_plots(img_np, score_np, active_mask_np, round_number, name)
 
-                active_mask_np = np.array(active_mask.cpu().numpy(), dtype=np.uint8)
-                active_mask_IMG = Image.fromarray(active_mask_np)
-                active_mask_IMG.save(path2mask[i])
-                indicator = {
-                    'active': active,
-                    'selected': selected
-                }
-                torch.save(indicator, path2indicator[i])
-
-            if cfg.ACTIVE.VIZ_MASK and idx in VIZ_LIST:
-                img_np = F.interpolate(tgt_input, size=size, mode='bilinear',
-                                       align_corners=True).cpu().numpy()[0].transpose(1, 2, 0)
-                img_np = (img_np * CITYSCAPES_STD + CITYSCAPES_MEAN).astype(np.uint8)
-                # active_mask_np = F.interpolate(torch.Tensor(active_mask_np).unsqueeze(0).unsqueeze(0), size=torch.Size(img_np.shape[:2]), mode='bilinear', align_corners=True)
-                # active_mask_np = active_mask_np.cpu().numpy().squeeze(0).squeeze(0)
-                score_np = score.cpu().numpy()
-                name = tgt_data['name'][0]
-                visualization_plots(img_np, score_np, active_mask_np, round_number, name)
-            idx += 1
+    with torch.no_grad():
+        # for tgt_data in tqdm(tgt_epoch_loader):
+        Parallel(n_jobs=8)(delayed(select_region)(tgt_data) for tgt_data in tqdm(tgt_epoch_loader))
 
     feature_extractor.train()
     classifier.train()
