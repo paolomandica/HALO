a944b1d14e0cfb12f44605bb2117632bcf5fd982
FraLuca <franco.luca.17@gmail.com>
2023-04-19 21:39:01 -0700

    add possibility to pretrain w/o imagenet weights

--- core/models/build.py
+++ core/models/build.py
@@ -7,8 +7,14 @@ import torch.nn as nn
 def build_feature_extractor(cfg):
     model_name, backbone_name = cfg.MODEL.NAME.split('_')
     if backbone_name.startswith('resnet'):
-        backbone = resnet_feature_extractor(backbone_name, pretrained_weights=cfg.MODEL.WEIGHTS, aux=False,
-                                            pretrained_backbone=True, freeze_bn=cfg.MODEL.FREEZE_BN)
+        if cfg.MODEL.WEIGHTS != 'none':
+            pretrained_backbone_flag = True
+        else:
+            pretrained_backbone_flag = False
+            pretrained_weights = None
+
+        backbone = resnet_feature_extractor(backbone_name, pretrained_weights=pretrained_weights, aux=False,
+                                            pretrained_backbone=pretrained_backbone_flag, freeze_bn=cfg.MODEL.FREEZE_BN)
     else:
         raise NotImplementedError
     return backbone
